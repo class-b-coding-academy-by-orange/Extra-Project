0566b5a9a22004b2160f4e7908b020ce6b7567b1
TahaAlTamimi <altamimitmo@gmail.com>
2019-08-18 12:37:02 +0300

    input field and take data

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,22 +12,47 @@ class Form extends Component {
         };
 
     }
+    clicke=()=>{
+        const name=document.getElementById("name").value;
+
+        console.log(name)
+
+        const email=document.getElementById("email").value;
+
+        console.log(email)
+
+       const phone= document.getElementById("phone").value;
+       console.log(phone)
+
+       const blog=document.getElementById("url").value
+       console.log(blog)
+       
+    }
+
     
     render() {
         return (
             <div className="row">
             <h1 className="text-center">Form Validation</h1>
             <form>
-                <h3>Name:
+                <h3>Name:<input id="name" type="text"/>
                 </h3>
-                <h3>Email:
+                <h3>Email:<input 
+                id="email"
+                type="email"/>
                 </h3>
-                <h3>Phone:
+                <h3>Phone:<input 
+                id="phone"
+                type="number"/>
                 </h3>
-                <h3>Blog URL:
+                <h3>Blog URL:<input 
+                id="url"
+                type="url"/>
                 </h3>
                 <div className="small-6 small-centered text-center columns">
-                    <a href="#" className="button success expand round text-center">Verify</a>
+                    <a href="#" 
+                    onClick={this.clicke}
+                    className="button success expand round text-center">Verify</a>
                 </div>
             </form>
         </div>);

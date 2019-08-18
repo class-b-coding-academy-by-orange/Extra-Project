03a4fbb2556b8da8a4a9fb56e509ca652502f133
sqoor <alsqoorAbdullah@gmail.com>
2019-08-18 12:03:00 +0300

    added input fields and styled it, Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
-import {PropTypes} from 'prop-types';
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import './Form.css';
 
 class Form extends Component {
     constructor(props) {
@@ -12,25 +13,33 @@ class Form extends Component {
         };
 
     }
-    
+
     render() {
         return (
             <div className="row">
-            <h1 className="text-center">Form Validation</h1>
-            <form>
-                <h3>Name:
-                </h3>
-                <h3>Email:
-                </h3>
-                <h3>Phone:
-                </h3>
-                <h3>Blog URL:
-                </h3>
-                <div className="small-6 small-centered text-center columns">
-                    <a href="#" className="button success expand round text-center">Verify</a>
-                </div>
-            </form>
-        </div>);
+                <h1 className="text-center">Form Validation</h1>
+                <form>
+                    <h3>
+                        Name: 
+                        <input rel={name => this.nameInput = name} type="text" placeholder="Enter your name" />
+                    </h3>
+                    <h3>
+                        Email: 
+                        <input rel={email => this.emailInput = email} type="email" placeholder="Enter your email" />
+                    </h3>
+                    <h3>
+                        Phone: 
+                        <input rel={phone => this.phoneInput = phone} type="phone" placeholder="Enter your phone number" />
+                    </h3>
+                    <h3>
+                        Blog URL: 
+                        <input rel={url => this.urlInput = url} type="url" placeholder="Enter your blog URL" />
+                    </h3>
+                    <div className="small-6 small-centered text-center columns">
+                        <a href="#" className="button success expand round text-center">Verify</a>
+                    </div>
+                </form>
+            </div>);
     }
 }
 

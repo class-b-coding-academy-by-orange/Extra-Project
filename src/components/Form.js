0b1dbb7307140eb79c31397c500c02d869c79de2
orayb-alsmadi <oraybalsmadi@yahoo.com>
2019-08-18 17:05:17 +0300

    react connections + print the validation msg

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,24 +13,54 @@ class Form extends Component {
 
     }
 
-    checkName = () =>
-    {
-
+    clickHandler = () =>{
+        console.log("hi")
+        if (this.state.isEmailValid&this.state.isNameValid&&this.state.isPhoneValid&&this.state.isUrlValid)
+        { this.props.msg(1)}
+        else
+        { this.props.msg(0)
+            // console.log(this.props.msg)
+        }
     }
 
-    checkEmail = () =>
-    {
-
+    checkName = async (e) =>
+    {      
+        let name = e.target.value;
+        let regex = /^[^\d]+$/
+        let result = regex.test(name)
+        console.log (result)
+        await this.setState({isNameValid:result})
+        // console.log (this.state)
     }
 
-    checkNumber = () =>
-    {
-
+    checkEmail = async (e) =>
+    {  
+        let name = e.target.value;
+        let regex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])+(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+        let result = regex.test(name)
+        console.log (result)
+        await this.setState({isEmailValid:result})
+        // console.log (this.state)
     }
 
-    checkUrl = () =>
+    checkNumber = async (e) =>
     {
+        let name = e.target.value;
+        let regex = /^\(?([2-9]{1})[0-9]{9}$/;
+        let result = regex.test(name)
+        console.log (result)
+        await this.setState({isPhoneValid:result})
+        // console.log (this.state)
+    }
 
+    checkUrl = async (e) =>
+    {
+        let name = e.target.value;
+        let regex = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/;
+        let result = regex.test(name)
+        console.log (result)
+        await this.setState({isUrlValid:result})
+        console.log (this.state)
     }
     
     render() {
@@ -51,7 +81,7 @@ class Form extends Component {
                 </h3>
                 <input type="url" id="url" name="url" placeholder="Enter your url" onChange={this.checkUrl}/> 
                 <div className="small-6 small-centered text-center columns">
-                    <a href="#" className="button success expand round text-center">Verify</a>
+                    <a href="#" className="button success expand round text-center" onClick={this.clickHandler}>Verify</a>
                 </div>
             </form>
         </div>);

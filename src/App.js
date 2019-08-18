967849aeba82b380a6dbd6691ef40e029827694e
AsmaAlfauri <asmaa.alfauri@gmail.com>
2019-08-18 17:16:09 +0300

    finish the react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, {Component} from 'react';
 import Form from './components/Form'
 import Message from './components/Message'
-
+var x = 0
 class App extends Component {
+    state={
+        aa:"validation isn't complete"
+    };
+    
+    verify=(email,name,url,phone)=>{
+        console.log(email)
+        console.log(name)
+        console.log(url)
+        console.log(phone)
+        if ((email & name & url & phone===true)){
+          this.setState({aa: "Completed"})
+          console.log("ALL IS True")
+            };
+             x=1;
+             console.log(x)
+          }
+          
+
 
     render() {
+        if(x===1){
+            return (<div>
+                <Form verify={this.verify}/>
+                <Message msg={this.state.aa}/>
+            </div>);
+        }
+        
         return (<div>
-            <Form></Form>
-            <Message></Message>
+            
+            <Form verify={this.verify}/>
+            <Message msg={this.state.aa}/>
         </div>);
     }
 }
